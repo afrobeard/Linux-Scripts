f793aeb98ce0bacb16237dc6dd5c79591a18e6fd
Iqbal Talaat Bhatti <iqbaltalaat@gmail.com>
2011-10-31 12:36:03 +0500

    Adding some Usage Information

--- Monitoring/memCSV.sh
+++ Monitoring/memCSV.sh
@@ -2,7 +2,12 @@
 
 if [ "$1" == "-h" ]
 then
-    echo "./memCSV <pid> <csvfile>"
+    echo "Usage:
+
+./memCSV <pid> <csvfile>
+
+Writes pid,%cpu,memory(kb),virtualmemory(kb) to CSV file
+"
 else
     for((i=0;;++i)) {
         ps -p $1 -o pid,pcpu,m_size,vsize | tail -n 1 | awk '{ $2 = $2; print }' | awk '{gsub(/ /,",");print}' >> $2;
