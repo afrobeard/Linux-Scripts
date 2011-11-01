c311e6db24d55fc20c9a33731b30c160295bf7ae
Iqbal Talaat Bhatti <iqbaltalaat@gmail.com>
2011-11-01 12:47:19 +0500

    Added in support for another CSV for catching Child Process ID's in another CSV file linked with a serial number

--- Monitoring/memCSV.sh
+++ Monitoring/memCSV.sh
@@ -2,15 +2,20 @@
 
 if [ "$1" == "-h" ]
 then
-    echo "Usage:
-
-./memCSV <pid> <csvfile>
-
-Writes pid,%cpu,memory(kb),virtualmemory(kb) to CSV file
-"
+    echo "./memCSV <pid> <csvfile> <optional:csvfilechildren>"
+elif [ "$1" == "" ]
+then
+    echo "./memCSV <pid> <csvfile> <optional:csvfilechildren>"
 else
-    for((i=0;;++i)) {
-        ps -p $1 -o pid,pcpu,m_size,vsize | tail -n 1 | awk '{ $2 = $2; print }' | awk '{gsub(/ /,",");print}' >> $2;
+    for((i=1;;++i)) {
+        ps -p $1 -o pid,pcpu,m_size,vsize,cmd | tail -n +2 | awk '{ $2 = $2; print }' | awk -v id=$i '{print id,$0}' | awk '{gsub(/ /,",");print}'  >> $2;
+	if [ "$3" = "" ]
+            then
+                #Do not Run csv file generation for children without 3rd argument
+                sleep 0
+            else
+                ps --ppid $1 -o pid,pcpu,m_size,vsize,cmd | tail -n +2 | awk '{ $2 = $2; print }' | awk -v id=$i '{print id,$0}' | awk '{gsub(/ /,",");print}'  >> $3;
+        fi
         sleep 5
     }
 fi
