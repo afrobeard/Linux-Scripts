72a523eb206c7d53cb73298d059d41bcd7fe218d
Iqbal Talaat Bhatti <iqbaltalaat@gmail.com>
2011-11-02 17:09:14 +0500

    Fixed to Use RSS which is real RAM depictor rather than m_size which is merely vsize divided by four

--- Monitoring/memCSV.sh
+++ Monitoring/memCSV.sh
@@ -8,7 +8,7 @@ then
     echo "./memCSV <pid> <csvfile> <optional:csvfilechildren>"
 else
     for((i=1;;++i)) {
-        ps -p $1 -o pid,pcpu,m_size,vsize,cmd | tail -n +2 | awk '{ $2 = $2; print }' | awk -v id=$i '{print id,$0}' | awk '{gsub(/ /,",");print}'  >> $2;
+        ps -p $1 -o pid,pcpu,rss,vsize,cmd | tail -n +2 | awk '{ $2 = $2; print }' | awk -v id=$i '{print id,$0}' | awk '{gsub(/ /,",");print}'  >> $2;
 	if [ "$3" = "" ]
             then
                 #Do not Run csv file generation for children without 3rd argument
