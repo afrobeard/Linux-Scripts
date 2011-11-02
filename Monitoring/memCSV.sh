6fc47f965a446262df2872653ce3e77e09910af9
Iqbal Talaat Bhatti <iqbaltalaat@gmail.com>
2011-11-02 17:14:09 +0500

    burp

--- Monitoring/memCSV.sh
+++ Monitoring/memCSV.sh
@@ -14,7 +14,7 @@ else
                 #Do not Run csv file generation for children without 3rd argument
                 sleep 0
             else
-                ps --ppid $1 -o pid,pcpu,m_size,vsize,cmd | tail -n +2 | awk '{ $2 = $2; print }' | awk -v id=$i '{print id,$0}' | awk '{gsub(/ /,",");print}'  >> $3;
+                ps --ppid $1 -o pid,pcpu,rss,vsize,cmd | tail -n +2 | awk '{ $2 = $2; print }' | awk -v id=$i '{print id,$0}' | awk '{gsub(/ /,",");print}'  >> $3;
         fi
         sleep 5
     }
