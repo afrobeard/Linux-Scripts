3ccab215a5d6a3d5f37bd5ef6c8ff04365a34225
Iqbal Talaat Bhatti <iqbaltalaat@gmail.com>
2011-11-22 11:15:32 +0500

    Added Support for Global VMStat to also keep tab on what is happening system level. May be useful in case some other program utilization interferes with testing

--- Monitoring/memCSV.sh
+++ Monitoring/memCSV.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" == "-h" ]
 then
-    echo "./memCSV <pid> <csvfile> <optional:csvfilechildren>"
+    echo "./memCSV <pid> <csvfile> <optional:csvfilechildren> <optional:statfile>"
 elif [ "$1" == "" ]
 then
-    echo "./memCSV <pid> <csvfile> <optional:csvfilechildren>"
+    echo "./memCSV <pid> <csvfile> <optional:csvfilechildren> <optional:statfile>"
 else
     for((i=1;;++i)) {
         ps -p $1 -o pid,pcpu,rss,vsize,cmd | tail -n +2 | awk '{ $2 = $2; print }' | awk -v id=$i '{print id,$0}' | awk '{gsub(/ /,",");print}'  >> $2;
@@ -15,6 +15,13 @@ else
                 sleep 0
             else
                 ps --ppid $1 -o pid,pcpu,rss,vsize,cmd | tail -n +2 | awk '{ $2 = $2; print }' | awk -v id=$i '{print id,$0}' | awk '{gsub(/ /,",");print}'  >> $3;
+        fi
+        if [ "$4" = "" ]
+            then
+                #Do not Run csv file generation for children without 3rd argument
+                sleep 0
+            else
+                vmstat | tail -1 | awk '{ $2 = $2; print }' | awk -v id=$i '{print id,$0}' | awk '{gsub(/ /,",");print}'  >> $4;
         fi
         sleep 5
     }
